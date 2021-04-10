8ff91a220832bd487132b1d02cc259418ec11cdb
AlisherUrazbayev <71267989+AlisherUrazbayev@users.noreply.github.com>
2021-04-10 02:41:31 -0400

    Delete labTest2-fullStack directory

--- labTest2-fullStack/models/mission.ts
+++
@@ -1,9 +0,0 @@
-export interface Mission {
-    mission_name: string;
-    launch_year: string;
-    links: any;
-    mission_patch_small: any;
-    details: string;
-    rocket: any;
-    launch_site: any;
-}
